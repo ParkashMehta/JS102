12cdc2ea1224332165e4eee7766240c1b0549e0a
Parkash Mehta <rharsh682@gmail.com>
2022-10-11 22:55:52 +0530

    First commit

---
+++ assignment-ft-web-23/project2.js
@@ -0,0 +1,9 @@
+let name="Parkash Mehta";
+console.log(name);
+
+
+let father_name="Surinder Mehta";
+console.log(father_name);
+
+let mother_name="Nirmala Devi";
+console.log(mother_name);
---
+++ assignment-ft-web-23/project4.js
@@ -0,0 +1,19 @@
+let name="Parkash Mehta";
+let school="Dav sr sec 8c chd";
+let grade="bsc cs";
+let section="A";
+let roll_no=1544;
+let english=98;
+let Hindi=95;
+let history=65;
+
+console.log("Student Name =",name); 
+console.log("School Name =",school);
+console.log("Grade =",grade);
+console.log("Section =",section);
+console.log("Roll No =",roll_no);
+
+      console.log("SUBJECT");
+console.log("English =",english);
+console.log("Hindi =",Hindi);
+console.log("History =",history);
