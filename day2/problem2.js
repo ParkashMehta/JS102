68f9e699644993d3e6696224cd0d58962dfc6e3b
Parkash Mehta <rharsh682@gmail.com>
2022-10-11 23:15:30 +0530

    second commit

---
+++ day2/problem2.js
@@ -0,0 +1,9 @@
+// Given any character, if it is a vowel print "Vowel"
+
+let char = "E";
+
+if (char == "a" || char == "A" || char == "e" || char == "E" || char == "i" || char == "I" || char == "" || char == "O" || char == "u" || char == "U") {
+  console.log("Vowel");
+} else {
+  console.log("consonant");
+}
---
+++ day2/problem3.js
@@ -0,0 +1,8 @@
+// Given and character if it is a consonant print "Consonant"
+let char = "G";
+
+if (char == "a" || char == "A" || char == "e" || char == "E" || char == "i" || char == "I" || char == "" || char == "O" || char == "u" || char == "U") {
+  console.log("Vowel");
+} else {
+  console.log("consonant");
+}
